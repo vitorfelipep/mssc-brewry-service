6c571055fe699cd23b113f23c07cd7f69518efde
Vitor Costa <vitor.costa@redspark.io>
2020-11-29 23:21:55 -0300

    initial project add

---
+++ src/main/java/com/microservice/springframework/guru/msscbreweryservice/MsscBreweryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.springframework.guru.msscbreweryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/springframework/guru/msscbreweryservice/MsscBreweryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.springframework.guru.msscbreweryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
